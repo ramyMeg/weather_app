496a96432ac92e6e617987ab578db12aa69b6f7c
ramyMeg <ramymegall@gmail.com>
2025-04-09 22:35:49 +0200

    Wearher BlocBuilder

--- lib/cubits/weather_cubit/weather_cubit.dart
+++ lib/cubits/weather_cubit/weather_cubit.dart
@@ -4,11 +4,13 @@ import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
 class WeatherCubit extends Cubit<WeatherState> {
-  WeatherCubit(this.weatherService) : super(WeatherLoading());
+
 
   WeatherService weatherService;
 
   WeatherModel? weatherModel;
+
+  WeatherCubit(this.weatherService):super(WeatherInitial());
   void getWeather({required String cityName}) async {
     emit(WeatherLoading());
     try {
--- lib/cubits/weather_cubit/weather_state.dart
+++ lib/cubits/weather_cubit/weather_state.dart
@@ -1,5 +1,5 @@
 abstract class WeatherState {}
-
+class WeatherInitial extends WeatherState{}
 class WeatherLoading extends WeatherState{}
 class WeatherSuccess extends WeatherState{}
 class WeatherFailure extends WeatherState{}
